a77787343a16e8046e73029b6afda52785cb24b0
Mirage <56789444+MirageCode@users.noreply.github.com>
2019-12-07 19:50:05 +0000

    Migrate Client to .NET Core

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -6,11 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Legend of Mir")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Legend of Mir")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,8 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.6.25.1919")]
-[assembly: AssemblyFileVersion("2017.6.25.1919")]
 
 [assembly: NeutralResourcesLanguage("en-GB")]
 
